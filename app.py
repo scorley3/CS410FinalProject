7de85906e729b6dd6d620893b3c3d5ef61b4b75c
scorley3 <scorley3@illinois.edu>
2024-04-28 15:35:38 -0500

    new commit

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask import Blueprint, render_template, send_from_directory
+from home.home import home_blueprint
+import os 
 
+os.environ['FLASK_DEBUG'] = 'True'
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+app.register_blueprint(home_blueprint)
+
+if __name__ == "__main__":
+    app.run(debug=True)
